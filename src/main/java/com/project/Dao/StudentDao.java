0735deb591fbf298f3cbceda743494cbae789042
juan <chavezvasquezjuan@gmail.com>
2019-08-12 15:34:56 -0500

    dao student

--- src/main/java/com/project/Dao/StudentDao.java
+++ src/main/java/com/project/Dao/StudentDao.java
@@ -1,4 +1,24 @@
 package com.project.Dao;
 
+import com.project.Entity.Student;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class StudentDao {
+    private static Map<Integer, Student> students;
+
+    static {
+        students = new HashMap<Integer, Student>(){
+            {
+                put(1, new Student(1, "Said", "course 1"));
+                put(2, new Student(1, "Said", "course 1"));
+                put(3, new Student(1, "Said", "course 1"));
+            }
+        };
+    }
+
+    public Collection<Student> getAllStudent() {
+        return students.values();
+    }
 }
